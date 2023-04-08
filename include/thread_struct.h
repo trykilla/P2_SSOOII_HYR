96e1acf2f8fc19cb57e8fa892b43a66be2974e4c
Rubencc02 <ruben.crespo.rcc@gmail.com>
2023-04-08 02:41:27 +0200

    Código comentado

--- include/thread_struct.h
+++ include/thread_struct.h
@@ -1,3 +1,13 @@
+
+/************************************************************************************
+ * Proyecto: Práctica 2 - Sistemas Operativos II                                    *
+ * Nombre del programa: thread_struct.h                                             *                                                       
+ * Autores: Héctor Alberca Sánchez-Quintanar y Rubén Crespo Calcerrada              *
+ * Fecha: 8/04/2023                                                                *
+ * Propósito: archivo con las diferentes estructuras que se van a utilizar          *
+ * a lo largo de la práctica.                                                       *   
+ ************************************************************************************/
+
 #include <iostream>
 #include <thread>
 #include <string>
@@ -6,12 +16,16 @@
 #ifndef THREAD_STRUCT_H
 #define THREAD_STRUCT_H
 
+/* Se trata de la estructura de cada una de las soluciones encontradas a lo largo de la búsqueda. */
 struct result_struct
 {
     std::string pre_word;
     std::string post_word;
     int line;
 };
+
+/* Se trata de la estructura de cada uno de los hilos que se van a crear. Cada uno tendrá una serie 
+    de estructuras de resultados, según los que haya encontrado en sus respectivas líneas. */
 struct thread_struct
 {
     int thread_id;
--- src/manager.cpp
+++ src/manager.cpp
@@ -1,3 +1,12 @@
+/************************************************************************************
+ * Proyecto: Práctica 2 - Sistemas Operativos II                                    *
+ * Nombre del programa: manager.cpp                                                 *                                                       
+ * Autores: Héctor Alberca Sánchez-Quintanar y Rubén Crespo Calcerrada              *
+ * Fecha: 8/04/2023                                                                *
+ * Propósito: Programa que gestiona los hilos                                       *   
+ ************************************************************************************/
+
+
 #include <iostream>
 #include <thread>
 #include <string>
@@ -9,9 +18,12 @@
 #include <chrono>
 #include <signal.h>
 
+/* Se definen las funciones que se van a utilizar */
 void find_word_in_file(std::string file_name, int i);
 void print_results(int threads_num, std::string word);
 void signal_handler(int signum);
+
+/*Se define tanto el semáforo compartido como el vector de estructuras de hilos*/
 
 std::shared_mutex mtx;
 
@@ -19,8 +31,14 @@ std::vector<thread_struct> v_thread_struct;
 
 int main(int argc, char const *argv[])
 {
+    /*  Se define el manejador de señal en caso de que el usuario quiera parar el programa y
+         se comprueban que los argumentos son los esperados   */
+
     signal(SIGINT, signal_handler);
     parse_args(argc);
+
+    /*  Se definen las variables nesarias para el programa y además se atribuyen una variable
+        a cada uno de los argumentos introducidos.     */
 
     std::vector<std::thread> v_threads;
 
@@ -36,8 +54,12 @@ int main(int argc, char const *argv[])
     int init_line = 0;
     int final_line = 0;
 
+    /*  Se crean los hilos y se les asigna una estructura de hilo con los datos necesarios para
+        su ejecución.   */
+
     for (int i = 0; i < threads_num; i++)
     {
+        //Se dividen las líneas entre los hilos
         id = i;
         init_line = i * lines_per_thread;
         final_line = init_line + lines_per_thread - 1;
@@ -48,9 +70,12 @@ int main(int argc, char const *argv[])
 
         v_thread_struct.push_back(thread_struct{id, init_line, final_line, word, std::queue<result_struct>()});
 
+        //Se crean los hilos y se añaden en el vector de hilos, a su vez se llama a la función find_word_in_file, 
+        //que es la que se encarga de buscar la palabra en el fichero.
         v_threads.push_back(std::thread(find_word_in_file, file_name, i));
     }
 
+    //Se espera a que todos los hilos terminen su ejecución y se imprime el resultado de cada uno de ellos.
     std::for_each(v_threads.begin(), v_threads.end(), [](std::thread &t)
                   { t.join(); });
 
@@ -59,17 +84,34 @@ int main(int argc, char const *argv[])
     return EXIT_SUCCESS;
 }
 
+/**
+ * Maneja las señales de interrupción del programa.
+ *
+ * @param signum Número de la señal
+ *
+ * @returns No devuelve nada
+ */
+
 void signal_handler(int signum)
 {
     std::cout << "\n[!] Interrupt signal (" << signum << ") received." << std::endl;
     exit(signum);
 }
 
+/**
+ * Imprime los resultados de la búsqueda de la palabra en el fichero.
+ *
+ * @param threads_num Número de hilos
+ * @param word Palabra a buscar
+ *
+ * @returns No devuelve nada
+ */
+
 void print_results(int threads_num, std::string word)
 {
     for (int i = 0; i < threads_num; i++)
     {
-
+        //Se imprime el resultado de cada hilo hasta que el vector de resultados de cada hilo esté vacío.    
         while (!v_thread_struct[i].result.empty())
         {
             std::cout << "[Hilo " << i << " inicio:" << v_thread_struct[i].initial_line << " - final: " << v_thread_struct[i].final_line << "]";
@@ -81,27 +123,43 @@ void print_results(int threads_num, std::string word)
     }
 }
 
+/**
+ * Busca la palabra en el fichero.
+ *
+ * @param file_name Nombre del fichero
+ * @param thread_v_pos Posición del vector de hilos
+ *
+ * @returns No devuelve nada
+ */
+
 void find_word_in_file(std::string file_name, int thread_v_pos)
 {
-
+    /*  Se inician y se crean las variables necesarias para la función. */
     int current_line = 0;
     std::fstream file;
     std::string line;
 
+    /*  Se abre el fichero y se lee línea a línea.   */
     file.open(file_name.c_str());
 
     std::string current_word, previous_word;
 
+    //Se convierte la palabra a mayúsculas para que no haya problemas con las mayúsculas y minúsculas.
     my_to_upper(v_thread_struct[thread_v_pos].word);
 
+    /* Se lee el fichero línea a línea hasta que se llega a la línea final del hilo que está realizando en este momento. */
     while (getline(file, line) && current_line <= v_thread_struct[thread_v_pos].final_line)
     {
         result_struct result_str;
         if (current_line >= v_thread_struct[thread_v_pos].initial_line)
         {
-            std::istringstream ss(line);
+            /*  Cogemos una línea y la dividimos en las palabras que tiene, vamos comprobando palabra por palabra 
+                si es la que estamos buscando. En caso de que sea esa, en la estructura result_struct guardamos 
+                la palabra anterior, la posterior y la línea donde la hemos encontrado.   */
+
+            std::istringstream words_in_line(line);
             std::string previous_word, current_word, next_word;
-            while (ss >> current_word)
+            while (words_in_line >> current_word)
             {
                 my_to_upper(current_word);
                 if (test_each_symbol(v_thread_struct[thread_v_pos].word, current_word))
@@ -109,10 +167,10 @@ void find_word_in_file(std::string file_name, int thread_v_pos)
 
                     my_to_lower(previous_word);
                     result_str.pre_word = previous_word;
-                    result_str.post_word = next_word;
+                    //result_str.post_word = next_word;
                     result_str.line = current_line + 1;
 
-                    if (ss >> next_word)
+                    if (words_in_line >> next_word)
                     {
 
                         result_str.post_word = next_word;
@@ -121,6 +179,8 @@ void find_word_in_file(std::string file_name, int thread_v_pos)
                     {
                         result_str.post_word = "[No hay palabra posterior]";
                     }
+                    /*  Utilizamos el semáforo para añadir información en el vector de estructuras de hilos compartidos. 
+                        De esta manera, no acceden varios semáforos al mismo tiempo al ser una sección crítica.*/
                     std::unique_lock<std::shared_mutex> lock(mtx);
                     v_thread_struct[thread_v_pos].result.push(result_str);
                     lock.unlock();
@@ -130,5 +190,6 @@ void find_word_in_file(std::string file_name, int thread_v_pos)
         }
         current_line++;
     }
+    //Se cierra el fichero.
     file.close();
 }
